1fc46d0c963e80145682592e332fabb165f016f2
jeongdopark <jdpark0816@gmail.com>
2020-12-09 23:05:00 +0900

    12/9

---
+++ DFS.py
@@ -0,0 +1,27 @@
+
+# DFS는 Depth-First Search, 깊이 우선 탐색이라고도 부르며, 그래프에서 깊은 부분을 우선적으로 탐색하는 알고리즘이다.
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+visited = [False] * 9
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end =' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+
+dfs(graph, 1, visited)
