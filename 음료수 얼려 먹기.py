5adfe25dd64e7b8f1527300a6faf48c42c1e45a2
jeongdopark <jdpark0816@gmail.com>
2020-12-14 23:56:37 +0900

    12/14

--- 음료수 얼려 먹기.py
+++ 음료수 얼려 먹기.py
@@ -2,6 +2,8 @@
 #  Depth First Search 
 #  틀린 답이다 이건.
 
+
+# return True의 의미 , 
 n, m = map(int, input().split())
 
 graph = []
@@ -9,19 +11,23 @@ for i in range(n):
     graph.append(list(map(int, input())))
 
 def dfs(x, y):
-    if x >= 0 or x < n or y >= 0 or y < m:
-        if graph[x][y] == 0:
-            graph[x][y] = 1
-            dfs(x - 1, y)
-            dfs(x, y -1)
-            dfs(x + 1 , y)
-            dfs(x, y + 1)
-            
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return False
+    if(graph[x][y] == 0):
+        graph[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x, y -1)
+        dfs(x + 1 , y)
+        dfs(x, y + 1)
+        return True
+    return False
+        
 
 result = 0
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 0:
+        if dfs(i, j) == True:
             result += 1
+            
 
 print(result)
