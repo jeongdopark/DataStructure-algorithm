7ae083f0a41b81cbaed60224033cb0dc2594490e
jeongdopark <jdpark0816@gmail.com>
2021-01-10 21:12:59 +0900

    1/10

--- 동적/강의가장높은탑쌓기.py
+++ 동적/강의가장높은탑쌓기.py
@@ -1,19 +1,20 @@
 
-
 n = int(input())
 
 num = [list(map(int,input().split())) for _ in range(n)]
 
 num = sorted(num, key = lambda x : x[0])
-print(num)
+
 mm = [0] * (n+1)
 mm[1] = num[0][1]
-
+ans = 0
 for i in range(2, n+1):
-    for j in range(i-1, 0, -1):
-        print(j)
+    for j in range(i-1, 0, -1):        
         if(num[i-1][2] > num[j-1][2]):
             mm[i] = num[i-1][1] + mm[j]
+            if(mm[i] > ans ):
+                ans = mm[i]
             break
+    
         
-print(mm)
+print(ans)
