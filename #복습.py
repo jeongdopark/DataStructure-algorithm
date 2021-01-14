b6afa9a115442d8eaea7683687f4876edd542124
jeongdopark <jdpark0816@gmail.com>
2021-01-14 19:42:54 +0900

    1/14

--- #복습.py
+++ #복습.py
@@ -1,1 +1,14 @@
 
+
+n = int(input())
+num = list(map(int, input().split()))
+m = int(input())
+num.sort()
+
+mm = [0] * (m+1)
+
+for i in range(n):
+    for j in range(num[i], m+1):
+        mm[j] = mm[j-num[i]] + 1
+print(mm[m])
+
