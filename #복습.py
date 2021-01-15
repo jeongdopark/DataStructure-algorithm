1ea68eb9e6f5d165d257af0785211c091f09411b
jeongdopark <jdpark0816@gmail.com>
2021-01-15 23:02:24 +0900

    1/15

--- #복습.py
+++ #복습.py
@@ -1,14 +1,33 @@
 
 
-n = int(input())
-num = list(map(int, input().split()))
-m = int(input())
+
+a, b = map(int, input().split())
+num = []
+for _ in range(a):
+    num.append(int(input()))
+
 num.sort()
 
-mm = [0] * (m+1)
+def count(n):
+    cnt = 1
+    flag = num[0]
+    for i in range(1, a):
+        if(flag + n <= num[i]):
+            flag = num[i]
+            cnt += 1
+    return cnt
 
-for i in range(n):
-    for j in range(num[i], m+1):
-        mm[j] = mm[j-num[i]] + 1
-print(mm[m])
 
+
+lt = num[0]
+rt = num[-1]
+ans = 0
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if(count(mid) < b):
+        rt = mid - 1
+    else:
+        ans = mid
+        lt = mid + 1
+
+print(ans)
